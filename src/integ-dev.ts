c9ef69a66951ac1b301b98081429c6aaf5e1f8e5
Neil Kuan <46012524+neilkuan@users.noreply.github.com>
2022-01-20 15:26:34 +0800

    chore: add cdk env config

--- src/integ-dev.ts
+++ src/integ-dev.ts
@@ -1,6 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { CdkCustomConstruct } from './index';
 
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
 const app = new cdk.App();
-const stack = new cdk.Stack(app, 'dev');
+const stack = new cdk.Stack(app, 'dev', { env: devEnv });
 new CdkCustomConstruct(stack, 'myconstruct');
+
+app.synth();
